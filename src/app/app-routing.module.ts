6791f152c808dafebeb4120938c2778c972e4841
Berkay Yıldız <berkay4421@gmail.com>
2023-12-10 18:13:27 +0300

    Set routing for about us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'about-us', component: AboutUsComponent},
   {path: '**', component: HomeComponent, redirectTo: ''},
 ];
 
