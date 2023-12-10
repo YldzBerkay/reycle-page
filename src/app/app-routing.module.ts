3b8e556a41ef343655a5db8a647548f5dfbdede8
Berkay Yıldız <berkay4421@gmail.com>
2023-12-10 19:03:25 +0300

    Set routing for communication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { CommunicationComponent } from './pages/communication/communication.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
   {path: 'about-us', component: AboutUsComponent},
+  {path: 'communication', component: CommunicationComponent},
   {path: '**', component: HomeComponent, redirectTo: ''},
 ];
 
